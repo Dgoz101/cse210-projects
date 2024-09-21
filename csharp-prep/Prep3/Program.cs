88d0822ac7e664badf5b8834c3a4abb72d5bf2f1
David Gosney <david.aphy@gmail.com>
2024-09-21 17:37:47 -0600

    Prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        
+        //Console.Write("Enter a magic number: ");
+        //int magicNumber = int.Parse(Console.ReadLine());
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1,101);
+        
+        int magicGuess = -1;
+
+        while (magicGuess != magicNumber)
+        {
+            Console.Write("What is your guess?");
+            magicGuess = int.Parse(Console.ReadLine());
+            
+            if (magicGuess > magicNumber)
+            {
+                Console.Write("Lower. Guess again. ");
+            }
+            else if (magicGuess < magicNumber)
+            {
+                Console.Write("Higher. Guess again. ");
+            }
+            else
+            {
+                Console.Write("You guessed it!");
+            }
+        }
+
+
+
     }
 }
