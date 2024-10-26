526ba7dee30f8595eedd0d614b2e88132ec2ecd1
David Gosney <david.aphy@gmail.com>
2024-10-26 17:49:31 -0600

    week6

--- prove/Develop06/Program.cs
+++ prove/Develop06/Program.cs
@@ -1,9 +1,40 @@
 using System;
+using System.IO;
 
 class Program
 {
     static void Main(string[] args)
+    {   
+    bool replay = true;  
+    GoalManager game = new GoalManager();
+    
+    while (replay)   
     {
-        Console.WriteLine("Hello Develop06 World!");
+       game.Start();
+
+       string choice = Console.ReadLine();
+
+        if (choice == "1")
+        {
+            game.CreateGoaL();
+        } else if (choice == "2")
+        {
+            game.ListGoalsDetails();
+        } else if (choice == "3") 
+        {
+            game.SaveGoals();
+        } else if (choice =="4")
+        {
+            game.LoadGoals();
+        } else if (choice == "5") 
+        {
+            game.RecordEvent();
+        } else if (choice == "6")
+        {
+            Console.Clear();
+            Console.WriteLine("Goodbye");
+            replay = false;   
+        }
+    }   
     }
 }
